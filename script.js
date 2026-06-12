afd0790e99f9fb760fb03074d2e35194f71c5686
darshan3266 <darshanmahale0077@gmail.com>
2026-06-12 10:30:22 +0530

    Add Luxora Website

---
+++ script.js
@@ -0,0 +1,14 @@
+
+const menuBox = document.getElementById("menuBox");
+function menuBar() {
+  if (menuBox.style.display === "none") {
+    menuBox.style.display = "block";
+
+
+  }
+  else{
+    menuBox.style.display="none"
+  }
+
+}
+
