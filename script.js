78583bfbb717ece8bdf61e5fa76db2b10f61cbc7
darshan3266 <darshanmahale0077@gmail.com>
2026-06-16 12:03:38 +0530

    scroll animation added

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+// console.log("js");
 
+
+//dropmenu btn for mobile responivenss
 const menuBox = document.getElementById("menuBox");
 function menuBar() {
   if (menuBox.style.display === "none") {
@@ -6,21 +9,41 @@ function menuBar() {
 
 
   }
-  else{
-    menuBox.style.display="none"
+  else {
+    menuBox.style.display = "none"
   }
 
 }
 
+//scrollBtn in homepage
+const scrollbtn = document.getElementById("scrollBtn");
 
-const scrollbtn=document.getElementById("scrollBtn");
-
-window.addEventListener("scroll",()=>{
-  if(window.scrollY>300){
-    scrollbtn.style.display="block";
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    scrollbtn.style.display = "block";
 
   }
-  else{
-    scrollbtn.style.display="none";
+  else {
+    scrollbtn.style.display = "none";
   }
 })
+
+
+const fadeConts = document.querySelectorAll(".fade-up");
+console.log(fadeConts);
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      console.log("Visible");
+      
+      entry.target.classList.add("show");
+    }
+  });
+},{
+  threshold:0.3
+  })
+  fadeConts.forEach((elements) => {
+    observer.observe(elements);
+
+});
